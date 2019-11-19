56bd6bb5eb837eab31385e055975f0cf22cd1e8f
Cheuk-Ming Tang <cheukkie@gmail.com>
2019-11-19 13:30:31 +0100

    - Mouseenter/leave functions with styling - Global functions to get all cells in column/row - Added function to add 1 to cell with styling and attribute mutations - Added click eventlistener to each cell

--- js/script.js
+++ js/script.js
@@ -3,6 +3,55 @@ const gridRows = 50;
 const gridCols = 50;
 const gridContainer = document.querySelector('#grid');
 
+
+function highlightColumn() {
+    const colPos = this.getAttribute('data-column');
+    const highlightedCols = document.querySelectorAll(`[data-column="${colPos}"]`);
+    highlightedCols.forEach((cell) => {
+        cell.classList.add('is-hover');
+    });
+}
+function resetColumns() {
+    const highlightedCols = document.querySelectorAll('td.is-hover');
+    highlightedCols.forEach((highlight) => {
+        highlight.classList.remove('is-hover');
+    });
+}
+function getRowCells(rowNr) {
+    return document.querySelectorAll(`td[data-row="${rowNr}"]`);
+}
+function getColCells(colNr) {
+    return document.querySelectorAll(`td[data-column="${colNr}"]`);
+}
+
+function addOne(cell) {
+    const currentNumber = parseInt(cell.getAttribute('data-num'), 10);
+    cell.classList.remove('is-empty');
+    cell.setAttribute('data-num', currentNumber + 1);
+    cell.innerText = currentNumber + 1;
+
+    cell.classList.add('is-clicked');
+    setTimeout(() => {
+        cell.classList.remove('is-clicked');
+    }, 500);
+}
+
+
+function clickCell(clickedCell) {
+    clickedCell.currentTarget.setAttribute('data-num', clickedCell.currentTarget.getAttribute('data-num') - 1);
+
+    const colId = clickedCell.currentTarget.getAttribute('data-column');
+    const rowId = clickedCell.currentTarget.getAttribute('data-row');
+    getRowCells(rowId).forEach((cell) => {
+        addOne(cell);
+    });
+
+    getColCells(colId).forEach((cell) => {
+        addOne(cell);
+    });
+}
+
+
 function fiboInit() {
     const t = document.createElement('TABLE');
 
@@ -12,13 +61,14 @@ function fiboInit() {
         t.appendChild(newRow);
         for (let cols = 0; cols < gridCols; cols++) {
             const newCell = document.createElement('td');
-            const newCellInner = document.createElement('div');
-            newCellInner.innerText = '1';
-            newCell.appendChild(newCellInner);
+
             newCell.classList.add('is-empty');
             newCell.setAttribute('data-column', cols);
             newCell.setAttribute('data-row', rows);
             newCell.setAttribute('data-num', 0);
+            newCell.addEventListener('click', clickCell);
+            newCell.addEventListener('mouseover', highlightColumn);
+            newCell.addEventListener('mouseleave', resetColumns);
 
             newRow.appendChild(newCell);
         }
