5e56ba2f300f44ab9295a635d929d8db74ca6749
Cheuk-Ming Tang <cheukkie@gmail.com>
2019-11-19 14:49:22 +0100

    Importing utility functions for a better overview and reusability

---
+++ js/utils.js
@@ -0,0 +1,14 @@
+export function isSquare(n) {
+    const s = Math.sqrt(n);
+    return s * s === n && n > 0 && Math.sqrt(n) % 1 === 0;
+}
+
+export function convertToArray(query) {
+    return Array.prototype.slice.call(query);
+}
+
+export function getRandomInt(minimum, maximum) {
+    const min = Math.ceil(minimum);
+    const max = Math.floor(maximum);
+    return Math.floor(Math.random() * (max - min)) + min; // The max is exclusive and the min is inclusive
+}
